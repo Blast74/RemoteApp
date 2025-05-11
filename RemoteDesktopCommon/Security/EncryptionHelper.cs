ca734ab8fa69d9342f149e621196a9ca9b389415
Blast74 <venzo.terence@gmail.com>
2025-05-11 20:14:55 +0200

    Add RemoteDesktopCommon/Security/EncryptionHelper.cs 1746987294003

--- RemoteDesktopCommon/Security/EncryptionHelper.cs
+++ RemoteDesktopCommon/Security/EncryptionHelper.cs
@@ -14,9 +14,9 @@ namespace RemoteDesktopCommon.Security
         private readonly byte[] _key;
         private readonly byte[] _iv;
 
-        public EncryptionHelper(ILogger<EncryptionHelper> logger, string encryptionKey = null)
+        public EncryptionHelper(ILogger<EncryptionHelper> logger, string? encryptionKey = null)
         {
-            _logger = logger;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _aes = Aes.Create();
             _aes.KeySize = ProtocolConstants.AES_KEY_SIZE;
             _aes.BlockSize = ProtocolConstants.AES_BLOCK_SIZE;
@@ -30,11 +30,13 @@ namespace RemoteDesktopCommon.Security
             }
             else
             {
-                using (var deriveBytes = new Rfc2898DeriveBytes(encryptionKey, new byte[8], 1000, HashAlgorithmName.SHA256))
-                {
-                    _key = deriveBytes.GetBytes(_aes.KeySize / 8);
-                    _iv = deriveBytes.GetBytes(_aes.BlockSize / 8);
-                }
+                using var deriveBytes = new Rfc2898DeriveBytes(
+                    encryptionKey, 
+                    new byte[8], 
+                    1000, 
+                    HashAlgorithmName.SHA256);
+                _key = deriveBytes.GetBytes(_aes.KeySize / 8);
+                _iv = deriveBytes.GetBytes(_aes.BlockSize / 8);
             }
 
             _aes.Key = _key;
@@ -43,19 +45,18 @@ namespace RemoteDesktopCommon.Security
 
         public byte[] Encrypt(byte[] data)
         {
+            ArgumentNullException.ThrowIfNull(data);
+
             try
             {
-                using (var msEncrypt = new MemoryStream())
-                {
-                    using (var encryptor = _aes.CreateEncryptor())
-                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                    {
-                        csEncrypt.Write(data, 0, data.Length);
-                        csEncrypt.FlushFinalBlock();
-                    }
+                using var msEncrypt = new MemoryStream();
+                using var encryptor = _aes.CreateEncryptor();
+                using var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+                
+                csEncrypt.Write(data, 0, data.Length);
+                csEncrypt.FlushFinalBlock();
 
-                    return msEncrypt.ToArray();
-                }
+                return msEncrypt.ToArray();
             }
             catch (Exception ex)
             {
@@ -66,16 +67,17 @@ namespace RemoteDesktopCommon.Security
 
         public byte[] Decrypt(byte[] encryptedData)
         {
+            ArgumentNullException.ThrowIfNull(encryptedData);
+
             try
             {
-                using (var msDecrypt = new MemoryStream(encryptedData))
-                using (var decryptor = _aes.CreateDecryptor())
-                using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                using (var msPlain = new MemoryStream())
-                {
-                    csDecrypt.CopyTo(msPlain);
-                    return msPlain.ToArray();
-                }
+                using var msDecrypt = new MemoryStream(encryptedData);
+                using var decryptor = _aes.CreateDecryptor();
+                using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+                using var msPlain = new MemoryStream();
+                
+                csDecrypt.CopyTo(msPlain);
+                return msPlain.ToArray();
             }
             catch (Exception ex)
             {
@@ -91,22 +93,16 @@ namespace RemoteDesktopCommon.Security
 
         private static byte[] GenerateRandomKey()
         {
-            using (var rng = new RNGCryptoServiceProvider())
-            {
-                var key = new byte[ProtocolConstants.AES_KEY_SIZE / 8];
-                rng.GetBytes(key);
-                return key;
-            }
+            var key = new byte[ProtocolConstants.AES_KEY_SIZE / 8];
+            RandomNumberGenerator.Fill(key);
+            return key;
         }
 
         private static byte[] GenerateRandomIV()
         {
-            using (var rng = new RNGCryptoServiceProvider())
-            {
-                var iv = new byte[ProtocolConstants.AES_BLOCK_SIZE / 8];
-                rng.GetBytes(iv);
-                return iv;
-            }
+            var iv = new byte[ProtocolConstants.AES_BLOCK_SIZE / 8];
+            RandomNumberGenerator.Fill(iv);
+            return iv;
         }
 
         public void Dispose()
@@ -120,28 +116,28 @@ namespace RemoteDesktopCommon.Security
         {
             public static (byte[] PublicKey, byte[] PrivateKey) GenerateKeyPair()
             {
-                using (var rsa = new RSACryptoServiceProvider(2048))
-                {
-                    return (rsa.ExportRSAPublicKey(), rsa.ExportRSAPrivateKey());
-                }
+                using var rsa = RSA.Create(2048);
+                return (rsa.ExportRSAPublicKey(), rsa.ExportRSAPrivateKey());
             }
 
             public static byte[] EncryptWithPublicKey(byte[] data, byte[] publicKey)
             {
-                using (var rsa = new RSACryptoServiceProvider())
-                {
-                    rsa.ImportRSAPublicKey(publicKey, out _);
-                    return rsa.Encrypt(data, RSAEncryptionPadding.OaepSHA256);
-                }
+                ArgumentNullException.ThrowIfNull(data);
+                ArgumentNullException.ThrowIfNull(publicKey);
+
+                using var rsa = RSA.Create();
+                rsa.ImportRSAPublicKey(publicKey, out _);
+                return rsa.Encrypt(data, RSAEncryptionPadding.OaepSHA256);
             }
 
             public static byte[] DecryptWithPrivateKey(byte[] encryptedData, byte[] privateKey)
             {
-                using (var rsa = new RSACryptoServiceProvider())
-                {
-                    rsa.ImportRSAPrivateKey(privateKey, out _);
-                    return rsa.Decrypt(encryptedData, RSAEncryptionPadding.OaepSHA256);
-                }
+                ArgumentNullException.ThrowIfNull(encryptedData);
+                ArgumentNullException.ThrowIfNull(privateKey);
+
+                using var rsa = RSA.Create();
+                rsa.ImportRSAPrivateKey(privateKey, out _);
+                return rsa.Decrypt(encryptedData, RSAEncryptionPadding.OaepSHA256);
             }
         }
     }
